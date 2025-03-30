923be1b91ddb5a428445dad983ffe1f169afe51f
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-03-30 15:19:03 +0330

    improve the logger

--- user-svc/slg/logger.go
+++ user-svc/slg/logger.go
@@ -3,6 +3,33 @@ package slg
 import (
 	"log/slog"
 	"os"
+	"strings"
 )
 
-var Logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+var Logger = initLogger()
+
+func initLogger() *slog.Logger {
+	level := slog.LevelInfo
+
+	if lvlStr, ok := os.LookupEnv("LOG_LEVEL"); ok {
+		switch strings.ToLower(lvlStr) {
+		case "debug":
+			level = slog.LevelDebug
+		case "info":
+			level = slog.LevelInfo
+		case "warn":
+			level = slog.LevelWarn
+		case "error":
+			level = slog.LevelError
+		}
+	}
+
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level:     level,
+		AddSource: true,
+	})
+
+	logger := slog.New(handler)
+	logger.Info("Logger initialized", "level", level.String())
+	return logger
+}
