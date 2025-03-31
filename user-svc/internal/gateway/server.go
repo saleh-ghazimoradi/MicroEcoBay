617845dbf81debe632a27b737123b6f0a281f83b
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-03-31 12:30:28 +0330

    implement grpc order service client with defining the order proto buff

--- user-svc/internal/gateway/server.go
+++ user-svc/internal/gateway/server.go
@@ -4,6 +4,9 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/saleh-ghazimoradi/MicroEcoBay/user_service/config"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/user_service/infra/queue"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/user_service/internal/gateway/grpc/order"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/user_service/slg"
 )
 
 func Server() error {
@@ -20,8 +23,15 @@ func Server() error {
 		})
 	})
 
-	//kafkaProducer := queue.NewProducer(config.AppConfig.KafkaConfig.Broker, config.AppConfig.KafkaConfig.Topic)
-	//slg.Logger.Info("Kafka producer created", "kafka", kafkaProducer)
+	kafkaProducer := queue.NewProducer(config.AppConfig.KafkaConfig.Broker, config.AppConfig.KafkaConfig.Topic)
+	slg.Logger.Info("Kafka producer created", "kafka", kafkaProducer)
+
+	orderServiceClient, err := order.NewGrpcOrderServiceClient("localhost:50051")
+	if err != nil {
+		slg.Logger.Error("Failed to create order service client", "error", err)
+	}
+
+	slg.Logger.Info("Order service client created", "client", orderServiceClient)
 
 	if err := app.Listen(config.AppConfig.ServerConfig.Port); err != nil {
 		return err
