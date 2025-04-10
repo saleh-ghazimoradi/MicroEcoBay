d9a8d198c1902a7423e2143737e01d51ade56137
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-04-10 14:25:50 +0330

    scaffold the product service

---
+++ product-svc/cmd/http.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/product_service/internal/gateway"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/product_service/slg"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// httpCmd represents the http command
+var httpCmd = &cobra.Command{
+	Use:   "http",
+	Short: "A brief description of your command",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("http called")
+		if err := gateway.Server(); err != nil {
+			slg.Logger.Error("the server encountered an error: " + err.Error())
+			os.Exit(1)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(httpCmd)
+}
---
+++ product-svc/cmd/root.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"github.com/saleh-ghazimoradi/MicroEcoBay/product_service/config"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/product_service/slg"
+	"github.com/spf13/cobra"
+	"os"
+	"time"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "product_service",
+	Short: "A brief description of your application",
+}
+
+func Execute() {
+	err := os.Setenv("TZ", time.UTC.String())
+	if err != nil {
+		panic(err)
+	}
+
+	err = rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+}
+
+func initConfig() {
+	err := config.LoadConfig()
+	if err != nil {
+		slg.Logger.Error("there went something wrong while loading config file", "error", err)
+	}
+}
