4121d9aa8bd92f6b9f613ea5a94c8c39572a0c81
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-04-10 14:05:36 +0330

    scaffold the payment service

---
+++ payment-svc/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/caarlos0/env/v11"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/payment_service/slg"
+)
+
+var AppConfig *Config
+
+type Config struct {
+	ServerConfig ServerConfig
+}
+
+type ServerConfig struct {
+	Port string `env:"SERVER_PORT"`
+}
+
+func LoadConfig() error {
+	config := &Config{}
+
+	if err := env.Parse(config); err != nil {
+		slg.Logger.Error("error loading config", "error", err)
+		return err
+	}
+	AppConfig = config
+
+	return nil
+}
---
+++ payment-svc/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/saleh-ghazimoradi/MicroEcoBay/payment_service/cmd"
+
+func main() {
+	cmd.Execute()
+}
