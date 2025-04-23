5b6baae76c66118b324e51a09a4d4f619047ae3f
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-04-23 18:32:43 +0330

    add validator tag to the dto and implement the validation

--- user-svc/internal/dto/user.go
+++ user-svc/internal/dto/user.go
@@ -1,12 +1,12 @@
 package dto
 
 type UserSignup struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Phone    string `json:"phone"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+	Phone    string `json:"phone" validate:"required,e164"`
 }
 
 type UserLogin struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
 }
