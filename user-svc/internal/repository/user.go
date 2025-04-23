418fe35467f9bcc52f63a4916108d873f75dc9a2
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-04-23 18:12:09 +0330

    improve error handling in the user repository layer and create some custom errors

--- user-svc/internal/repository/user.go
+++ user-svc/internal/repository/user.go
@@ -2,7 +2,10 @@ package repository
 
 import (
 	"context"
+	"errors"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/user_service/internal/customErr"
 	"github.com/saleh-ghazimoradi/MicroEcoBay/user_service/internal/domain"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/user_service/slg"
 	"gorm.io/gorm"
 )
 
@@ -20,25 +23,50 @@ type userRepository struct {
 }
 
 func (u *userRepository) CreateUser(ctx context.Context, user *domain.User) error {
-	return u.dbWrite.WithContext(ctx).Create(user).Error
+	err := u.dbWrite.WithContext(ctx).Create(user).Error
+	if err != nil {
+		switch {
+		case errors.Is(err, gorm.ErrDuplicatedKey):
+			slg.Logger.Error("failed to create user: duplicate email", "error", err.Error())
+			return customErr.ErrDuplicateUser
+		default:
+			return err
+		}
+	}
+	return nil
 }
 
 func (u *userRepository) FindUserByEmail(ctx context.Context, email string) (*domain.User, error) {
 	var user domain.User
 	if err := u.dbRead.WithContext(ctx).Preload("Address").First(&user, "email = ?", email).Error; err != nil {
-		return nil, err
+		switch {
+		case errors.Is(err, gorm.ErrRecordNotFound):
+			slg.Logger.Warn("user not found", "email", email)
+			return nil, customErr.ErrUserNotFound
+		default:
+			return nil, err
+		}
 	}
 	return &user, nil
 }
 
 func (u *userRepository) SaveUser(ctx context.Context, user *domain.User) error {
-	return u.dbWrite.WithContext(ctx).Save(user).Error
+	if err := u.dbWrite.WithContext(ctx).Save(user).Error; err != nil {
+		slg.Logger.Error("failed to save user", "error", err.Error())
+	}
+	return nil
 }
 
 func (u *userRepository) FindUserByResetToken(ctx context.Context, token string) (*domain.User, error) {
 	var user domain.User
 	if err := u.dbRead.WithContext(ctx).Where("reset_token = ?", token).First(&user).Error; err != nil {
-		return nil, err
+		switch {
+		case errors.Is(err, gorm.ErrRecordNotFound):
+			slg.Logger.Warn("user not found", "token", token)
+			return nil, customErr.ErrInvalidUserResetToken
+		default:
+			return nil, err
+		}
 	}
 	return &user, nil
 }
@@ -46,7 +74,13 @@ func (u *userRepository) FindUserByResetToken(ctx context.Context, token string)
 func (u *userRepository) FindUserById(ctx context.Context, id uint) (*domain.User, error) {
 	var user domain.User
 	if err := u.dbRead.WithContext(ctx).First(&user, id).Error; err != nil {
-		return nil, err
+		switch {
+		case errors.Is(err, gorm.ErrRecordNotFound):
+			slg.Logger.Warn("user not found", "id", id)
+			return nil, customErr.ErrUserNotFound
+		default:
+			return nil, err
+		}
 	}
 	return &user, nil
 }
