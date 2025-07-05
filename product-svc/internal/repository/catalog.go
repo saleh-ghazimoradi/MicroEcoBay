7dddb5f2770915058edbe54391a23a2bf27ceeef
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-07-05 18:45:24 +0330

    improve the catalog repository by adding logger and suitable error handling method

--- product-svc/internal/repository/catalog.go
+++ product-svc/internal/repository/catalog.go
@@ -2,7 +2,10 @@ package repository
 
 import (
 	"context"
+	"errors"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/product_service/internal/customErr"
 	"github.com/saleh-ghazimoradi/MicroEcoBay/product_service/internal/domain"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/product_service/slg"
 	"gorm.io/gorm"
 )
 
@@ -24,13 +27,18 @@ type catalogRepository struct {
 }
 
 func (c *catalogRepository) CreateCategory(ctx context.Context, category *domain.Category) error {
-	return c.dbWrite.WithContext(ctx).Create(category).Error
+	if err := c.dbWrite.WithContext(ctx).Create(category).Error; err != nil {
+		slg.Logger.Error("error on creating category", "error", err.Error())
+		return customErr.ErrCatalogCreate
+	}
+	return nil
 }
 
 func (c *catalogRepository) GetAllCategories(ctx context.Context) ([]*domain.Category, error) {
 	var categories []*domain.Category
 	if err := c.dbRead.WithContext(ctx).Find(&categories).Error; err != nil {
-		return nil, err
+		slg.Logger.Error("error on getting all categories", "error", err.Error())
+		return nil, customErr.ErrCatalogGet
 	}
 	return categories, nil
 }
@@ -38,23 +46,37 @@ func (c *catalogRepository) GetAllCategories(ctx context.Context) ([]*domain.Cat
 func (c *catalogRepository) GetCategoryById(ctx context.Context, id uint) (*domain.Category, error) {
 	var category domain.Category
 	if err := c.dbRead.WithContext(ctx).First(&category, id).Error; err != nil {
-		return nil, err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			slg.Logger.Error("category not found", "id", id)
+			return nil, customErr.ErrNotFound
+		}
+		slg.Logger.Error("error on getting category", "error", err.Error())
+		return nil, customErr.ErrCatalogGet
 	}
 	return &category, nil
 }
 
 func (c *catalogRepository) UpdateCategory(ctx context.Context, id uint, update *domain.Category) error {
-	return c.dbWrite.WithContext(ctx).Model(&domain.Category{}).Where("id = ?", id).Updates(update).Error
+	if err := c.dbWrite.WithContext(ctx).Model(&domain.Category{}).Where("id = ?", id).Updates(update).Error; err != nil {
+		slg.Logger.Error("error on updating category", "error", err.Error())
+		return customErr.ErrUpdateCatalog
+	}
+	return nil
 }
 
 func (c *catalogRepository) CreateProduct(ctx context.Context, product *domain.Product) error {
-	return c.dbWrite.WithContext(ctx).Create(product).Error
+	if err := c.dbWrite.WithContext(ctx).Create(product).Error; err != nil {
+		slg.Logger.Error("error on creating product", "error", err.Error())
+		return customErr.ErrProductCreate
+	}
+	return nil
 }
 
 func (c *catalogRepository) GetAllProducts(ctx context.Context) ([]*domain.Product, error) {
 	var products []*domain.Product
 	if err := c.dbRead.WithContext(ctx).Find(&products).Error; err != nil {
-		return nil, err
+		slg.Logger.Error("error on getting all products", "error", err.Error())
+		return nil, customErr.ErrProductGet
 	}
 	return products, nil
 }
@@ -62,13 +84,21 @@ func (c *catalogRepository) GetAllProducts(ctx context.Context) ([]*domain.Produ
 func (c *catalogRepository) GetProductById(ctx context.Context, id uint) (*domain.Product, error) {
 	var product domain.Product
 	if err := c.dbRead.WithContext(ctx).First(&product, id).Error; err != nil {
-		return nil, err
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			slg.Logger.Error("product not found", "id", id)
+			return nil, customErr.ErrProductGet
+		}
+		return nil, customErr.ErrProductGet
 	}
 	return &product, nil
 }
 
 func (c *catalogRepository) UpdateProduct(ctx context.Context, id uint, update *domain.Product) error {
-	return c.dbWrite.WithContext(ctx).Model(&domain.Product{}).Where("id = ?", id).Updates(update).Error
+	if err := c.dbWrite.WithContext(ctx).Model(&domain.Product{}).Where("id = ?", id).Updates(update).Error; err != nil {
+		slg.Logger.Error("error on updating product", "error", err.Error())
+		return customErr.ErrProductUpdate
+	}
+	return nil
 }
 
 func NewCatalogRepository(dbWrite, dbRead *gorm.DB) CatalogRepository {
