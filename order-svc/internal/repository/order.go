219d4f6bc00c3e15746ee30f1470aa88057c6f0b
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-11-16 18:46:41 +0330

    implement the order repository functionalities

---
+++ order-svc/internal/repository/order.go
@@ -0,0 +1,50 @@
+package repository
+
+import (
+	"context"
+	"github.com/saleh-ghazimoradi/MicroEcoBay/order_service/internal/domain"
+	"gorm.io/gorm"
+)
+
+type OrderRepository interface {
+	CreateOrder(ctx context.Context, order *domain.Order) error
+	GetOrderByUser(ctx context.Context, userId uint) ([]*domain.Order, error)
+	GetOrderById(ctx context.Context, orderId, userId uint) (*domain.Order, error)
+}
+
+type orderRepository struct {
+	dbWrite *gorm.DB
+	dbRead  *gorm.DB
+}
+
+func (o *orderRepository) CreateOrder(ctx context.Context, order *domain.Order) error {
+	return o.dbWrite.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Create(order).Error; err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func (o *orderRepository) GetOrderByUser(ctx context.Context, userId uint) ([]*domain.Order, error) {
+	var orders []*domain.Order
+	if err := o.dbRead.Preload("OrderItems").Where("user_id = ?", userId).Find(&orders).Error; err != nil {
+		return nil, err
+	}
+	return orders, nil
+}
+
+func (o *orderRepository) GetOrderById(ctx context.Context, orderId, userId uint) (*domain.Order, error) {
+	var order *domain.Order
+	if err := o.dbRead.Preload("OrderItems").Where("order_id = ? AND user_id = ?", orderId, userId).First(&order).Error; err != nil {
+		return nil, err
+	}
+	return order, nil
+}
+
+func NewOrderRepository(dbWrite, dbRead *gorm.DB) OrderRepository {
+	return &orderRepository{
+		dbWrite: dbWrite,
+		dbRead:  dbRead,
+	}
+}
