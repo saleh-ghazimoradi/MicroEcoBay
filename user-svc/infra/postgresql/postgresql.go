0daf5b3b16a1e0afcbcea1425d665893d0df5b42
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-12-14 19:52:25 +0330

    improve my postgresql connection using functional options pattern

---
+++ user-svc/infra/postgresql/postgresql.go
@@ -0,0 +1,134 @@
+package postgresql
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/rs/zerolog"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"time"
+)
+
+type Postgresql struct {
+	Host        string
+	Port        string
+	User        string
+	Password    string
+	Name        string
+	MaxOpenConn int
+	MaxIdleConn int
+	MaxIdleTime time.Duration
+	SSLMode     string
+	Timeout     time.Duration
+	logger      *zerolog.Logger
+}
+
+type Options func(*Postgresql)
+
+func WithHost(host string) Options {
+	return func(p *Postgresql) {
+		p.Host = host
+	}
+}
+
+func WithPort(port string) Options {
+	return func(p *Postgresql) {
+		p.Port = port
+	}
+}
+
+func WithUser(user string) Options {
+	return func(p *Postgresql) {
+		p.User = user
+	}
+}
+
+func WithPassword(password string) Options {
+	return func(p *Postgresql) {
+		p.Password = password
+	}
+}
+
+func WithName(name string) Options {
+	return func(p *Postgresql) {
+		p.Name = name
+	}
+}
+
+func WithMaxOpenConn(maxOpenConn int) Options {
+	return func(p *Postgresql) {
+		p.MaxOpenConn = maxOpenConn
+	}
+}
+
+func WithMaxIdleConn(maxIdleConn int) Options {
+	return func(p *Postgresql) {
+		p.MaxIdleConn = maxIdleConn
+	}
+}
+
+func WithMaxIdleTime(maxIdleTime time.Duration) Options {
+	return func(p *Postgresql) {
+		p.MaxIdleTime = maxIdleTime
+	}
+}
+
+func WithSSLMode(mode string) Options {
+	return func(p *Postgresql) {
+		p.SSLMode = mode
+	}
+}
+
+func WithTimeout(timeout time.Duration) Options {
+	return func(p *Postgresql) {
+		p.Timeout = timeout
+	}
+}
+
+func WithLogger(logger *zerolog.Logger) Options {
+	return func(p *Postgresql) {
+		p.logger = logger
+	}
+}
+
+func (p *Postgresql) uri() string {
+	return fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=UTC", p.Host, p.User, p.Password, p.Name, p.Port, p.SSLMode)
+}
+
+func (p *Postgresql) Connect() (*gorm.DB, *sql.DB, error) {
+	db, err := gorm.Open(postgres.Open(p.uri()), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		p.logger.Fatal().Err(err).Msg("failed to connect database")
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		p.logger.Fatal().Err(err).Msg("failed to get sql.DB")
+	}
+
+	sqlDB.SetMaxOpenConns(p.MaxOpenConn)
+	sqlDB.SetMaxIdleConns(p.MaxIdleConn)
+	sqlDB.SetConnMaxIdleTime(p.MaxIdleTime)
+
+	ctx, cancel := context.WithTimeout(context.Background(), p.Timeout)
+	defer cancel()
+
+	if err = sqlDB.PingContext(ctx); err != nil {
+		sqlDB.Close()
+		p.logger.Fatal().Err(err).Msg("failed to ping database")
+	}
+
+	return db, sqlDB, nil
+}
+
+func NewPostgresql(opts ...Options) *Postgresql {
+	p := &Postgresql{}
+	for _, opt := range opts {
+		opt(p)
+	}
+	return p
+}
