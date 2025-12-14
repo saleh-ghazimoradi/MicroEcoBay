2e9c4abf3fcb79891b5672f6b5a3190573c63de7
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-12-14 22:44:41 +0330

    remove unnecessary logs

--- user-svc/internal/service/user.go
+++ user-svc/internal/service/user.go
@@ -8,7 +8,6 @@ import (
 	"github.com/saleh-ghazimoradi/MicroEcoBay/user_service/internal/domain"
 	"github.com/saleh-ghazimoradi/MicroEcoBay/user_service/internal/dto"
 	"github.com/saleh-ghazimoradi/MicroEcoBay/user_service/internal/repository"
-	"github.com/saleh-ghazimoradi/MicroEcoBay/user_service/slg"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -55,7 +54,6 @@ func (u *userService) Register(ctx context.Context, input *dto.UserSignup) error
 func (u *userService) Login(ctx context.Context, input *dto.UserLogin) (*domain.User, error) {
 	user, err := u.findUserByEmail(ctx, input.Email)
 	if user == nil || err != nil {
-		slg.Logger.Warn("invalid email or password")
 		return nil, errors.New("invalid email or password")
 	}
 
@@ -69,20 +67,17 @@ func (u *userService) Login(ctx context.Context, input *dto.UserLogin) (*domain.
 func (u *userService) ForgotPassword(ctx context.Context, input *dto.ForgotPassword) error {
 	user, err := u.findUserByEmail(ctx, input.Email)
 	if err != nil {
-		slg.Logger.Warn("user not found")
 		return err
 	}
 
 	resetToken, err := generateHashedPassword(user.Email)
 	if err != nil {
-		slg.Logger.Error("error generating reset token", "error", err)
 		return errors.New("error generating reset token")
 	}
 
 	user.ResetToken = resetToken
 
 	if err := u.userRepository.SaveUser(ctx, user); err != nil {
-		slg.Logger.Error("error saving user", "error", err)
 		return err
 	}
 
@@ -92,13 +87,11 @@ func (u *userService) ForgotPassword(ctx context.Context, input *dto.ForgotPassw
 func (u *userService) SetPassword(ctx context.Context, input *dto.SetPassword) error {
 	user, err := u.userRepository.FindUserByResetToken(ctx, input.Token)
 	if err != nil {
-		slg.Logger.Warn("invalid or expired token")
 		return errors.New("invalid or expired token")
 	}
 
 	hashedPassword, err := generateHashedPassword(input.Password)
 	if err != nil {
-		slg.Logger.Error("error generating hashed password", "error", err)
 		return err
 	}
 
@@ -106,7 +99,6 @@ func (u *userService) SetPassword(ctx context.Context, input *dto.SetPassword) e
 	user.ResetToken = ""
 
 	if err = u.userRepository.SaveUser(ctx, user); err != nil {
-		slg.Logger.Error("error saving user", "error", err)
 		return err
 	}
 
@@ -116,7 +108,6 @@ func (u *userService) SetPassword(ctx context.Context, input *dto.SetPassword) e
 func (u *userService) CreateProfile(ctx context.Context, profile *dto.UserProfile) error {
 	user, err := u.findUserById(ctx, profile.UserId)
 	if err != nil {
-		slg.Logger.Warn("user not found")
 		return err
 	}
 
@@ -157,7 +148,6 @@ func (u *userService) CreateProfile(ctx context.Context, profile *dto.UserProfil
 	}
 
 	if err = u.userRepository.SaveUser(ctx, user); err != nil {
-		slg.Logger.Error("error saving user", "error", err)
 		return err
 	}
 
@@ -167,7 +157,6 @@ func (u *userService) CreateProfile(ctx context.Context, profile *dto.UserProfil
 func (u *userService) GetProfile(ctx context.Context, id uint) (*domain.User, error) {
 	user, err := u.findUserById(ctx, id)
 	if err != nil {
-		slg.Logger.Warn("user not found")
 		return nil, err
 	}
 	return user, nil
@@ -177,7 +166,6 @@ func (u *userService) Authenticate(ctx *fiber.Ctx) (*domain.User, error) {
 	user := ctx.Locals("userId")
 	authUser, err := u.findUserById(ctx.Context(), user.(uint))
 	if err != nil {
-		slg.Logger.Warn("user not found")
 		return nil, err
 	}
 
@@ -187,7 +175,6 @@ func (u *userService) Authenticate(ctx *fiber.Ctx) (*domain.User, error) {
 func generateHashedPassword(password string) (string, error) {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
-		slg.Logger.Error("error hashing password", "error", err.Error())
 		return "", errors.New("error generating hashed password")
 	}
 	return string(hashedPassword), nil
@@ -195,7 +182,6 @@ func generateHashedPassword(password string) (string, error) {
 
 func verifyPassword(plainPassword string, hashedPassword string) error {
 	if err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword)); err != nil {
-		slg.Logger.Error("error verifying password", "error", err.Error())
 		return errors.New("password does not match")
 	}
 	return nil
